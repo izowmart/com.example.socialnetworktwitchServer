aa573849504f86e8aa1b265dd6cbeb0be31f38cc
Zack <izowmart7@gmail.com>
2023-02-01 12:03:10 +0300

    - Implemented userRepository

--- src/main/kotlin/com/example/data/repository/user/UserRepositoryImpl.kt
+++ src/main/kotlin/com/example/data/repository/user/UserRepositoryImpl.kt
@@ -2,23 +2,25 @@ package com.example.data.repository.user
 
 import com.example.data.models.User
 import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.coroutine.insertOne
+import org.litote.kmongo.eq
 
 class UserRepositoryImpl(
     db: CoroutineDatabase
-): UserRepository {
+) : UserRepository {
 
     private val users = db.getCollection<User>()
 
     override suspend fun createUser(user: User) {
-        TODO("Not yet implemented")
+        users.insertOne(user)
     }
 
     override suspend fun getUserById(id: String): User? {
-        TODO("Not yet implemented")
+        return users.findOne(User::id eq id)
     }
 
     override suspend fun getUserByEmail(email: String): User? {
-        TODO("Not yet implemented")
+       return users.findOne(User::email eq email)
     }
 
     override suspend fun doesPasswordForUserMatch(email: String, enteredPassword: String): Boolean {
@@ -36,6 +38,7 @@ class UserRepositoryImpl(
     override suspend fun getUsers(userIds: List<String>): List<User> {
         TODO("Not yet implemented")
     }
+
     override suspend fun updateUser(
         userId: String,
         profileImageUrl: String?,
