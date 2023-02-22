c21c50398b6841df5fc54695a5c13a3a65704eb3
Zack <izowmart7@gmail.com>
2023-02-22 17:05:46 +0300

    - Implemented activityService and route route

--- src/main/kotlin/com/example/service/ActivityService.kt
+++ src/main/kotlin/com/example/service/ActivityService.kt
@@ -2,28 +2,32 @@ package com.example.service
 
 import com.example.data.models.Activity
 import com.example.data.repository.activity.ActivityRepository
+import com.example.data.repository.comment.CommentRepository
 import com.example.data.repository.post.PostRepository
 import com.example.data.responses.ActivityResponse
+import com.example.data.util.ActivityType
+import com.example.data.util.ParentType
 import com.example.util.Constants.DEFAULT_ACTIVITY_PAGE_SIZE
 
 class ActivityService(
     private val activityRepository: ActivityRepository,
-    private val postRepository: PostRepository
+    private val postRepository: PostRepository,
+    private val commentRepository: CommentRepository
 ) {
     suspend fun getActivitiesForUser(
         userId: String,
         page: Int = 0,
         pageSize: Int = DEFAULT_ACTIVITY_PAGE_SIZE
-    ): List<ActivityResponse>{
-        return activityRepository.getActivitiesForUser(userId,page,pageSize)
+    ): List<ActivityResponse> {
+        return activityRepository.getActivitiesForUser(userId, page, pageSize)
     }
 
     suspend fun addCommentActivity(
         byUserId: String,
         postId: String
-    ):Boolean{
-        val userIdOfPost = postRepository.getPost(postId) ?: return false
-        if (byUserId == userIdOfPost){ // todo
+    ): Boolean {
+        val userIdOfPost = postRepository.getPost(postId)?.userId ?: return false
+        if (byUserId == userIdOfPost) { // todo
             return false
         }
         activityRepository.createActivity(
@@ -31,7 +35,7 @@ class ActivityService(
                 timestamp = System.currentTimeMillis(),
                 byUserId = byUserId,
                 toUserId = userIdOfPost,
-                type = 1,
+                type = ActivityType.CommentedOnPost.type,
                 parentId = postId
             )
         )
@@ -43,18 +47,43 @@ class ActivityService(
         byUserId: String,
         parentType: ParentType,
         parentId: String
-    ): Boolean{
-        val toUserId = when(parentType){
+    ): Boolean {
+        val toUserId = when (parentType) {
             is ParentType.Post -> {
-
+                postRepository.getPost(parentId)?.userId
             }
+            is ParentType.Comment -> {
+                commentRepository.getComment(parentId)?.userId
+            }
+            is ParentType.None -> {
+               return false
+            }
+        } ?: return false
+
+        if (byUserId == toUserId){
+            return false
         }
+        activityRepository.createActivity(
+            Activity(
+                timestamp = System.currentTimeMillis(),
+                byUserId = byUserId,
+                toUserId = toUserId,
+                type = when(parentType){
+                    is ParentType.Post -> ActivityType.LikedPost.type
+                    is ParentType.Comment -> ActivityType.LikedComment.type
+                    else -> ActivityType.LikedPost.type
+                },
+                parentId = parentId
+            )
+        )
+        return true
     }
 
-    suspend fun createActivity(activity: Activity){
+    suspend fun createActivity(activity: Activity) {
         activityRepository.createActivity(activity)
     }
-    suspend fun deleteActivity(activityId: String): Boolean{
+
+    suspend fun deleteActivity(activityId: String): Boolean {
         return activityRepository.deleteActivity(activityId)
     }
 }
