a1dbeba17f9a61fab9e84705a7e717c9918ab372
Zack <izowmart7@gmail.com>
2023-02-22 17:19:47 +0300

    - Working on postRou implementation

--- src/main/kotlin/com/example/routes/ActivityRoutes.kt
+++ src/main/kotlin/com/example/routes/ActivityRoutes.kt
@@ -9,7 +9,6 @@ import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-
 fun Route.getActivities(
     activityService: ActivityService
 ) {
--- src/main/kotlin/com/example/routes/PostRoutes.kt
+++ src/main/kotlin/com/example/routes/PostRoutes.kt
@@ -6,19 +6,47 @@ import com.example.service.LikeService
 import com.example.service.PostService
 import com.example.util.Constants
 import com.example.util.QueryParams
+import com.example.util.save
+import com.google.gson.Gson
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 import java.io.File
 
 fun Route.createPost(
     postService: PostService,
 ) {
+    val gson by inject<Gson>()
     authenticate {
         post("/api/post/create") {
-            val createPostRequest: CreatePostRequest? = null
+            val multipart = call.receiveMultipart()
+            var createPostRequest: CreatePostRequest? = null
+            var fileName: String? = null
+
+            multipart.forEachPart { partData ->
+                when(partData){
+                    is PartData.FormItem ->{
+                        if (partData.name == "post_data"){
+                            createPostRequest = gson.fromJson(
+                                partData.value,
+                                CreatePostRequest::class.java
+                            )
+                        }
+                    }
+                    is PartData.FileItem ->{
+                        fileName = partData.save(Constants.POST_PICTURE_PATH)
+                    }
+                    is PartData.BinaryItem -> Unit
+                    else -> {Unit}
+                }
+            }
+
+            val postPictureUrl = "${Constants.BASE_URL}post_pictures/$fileName"
 
             createPostRequest?.let { request ->
                 val createPostAcknowledgement = postService.createPost(
