91709a9bccc2be8f2fb8ab2e542c292d2b0e3747
Zack <izowmart7@gmail.com>
2023-03-14 12:47:53 +0300

    - Started implementing chat application

--- src/main/kotlin/com/example/routes/ChatRoute.kt
+++ src/main/kotlin/com/example/routes/ChatRoute.kt
@@ -1,53 +1,105 @@
 package com.example.routes
 
-
 import com.example.service.chat.ChatController
 import com.example.service.chat.ChatService
 import com.example.util.Constants
 import com.example.util.QueryParams
+import com.example.util.WebSocketObject
+import com.example.util.fromJsonOrNull
+import com.google.gson.Gson
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.consumeEach
+import org.koin.java.KoinJavaComponent.inject
 
-fun Route.getMessagesForChat(chatService: ChatService){
-    authenticate{
-        get("/api/chat/messages"){
+fun Route.getMessagesForChat(chatService: ChatService) {
+    authenticate {
+        get("/api/chat/messages") {
             val chatId = call.parameters[QueryParams.PARAM_CHAT_ID] ?: kotlin.run {
                 call.respond(HttpStatusCode.BadRequest)
                 return@get
             }
 
             val page = call.parameters[QueryParams.PARAM_PAGE]?.toIntOrNull() ?: 0
-            val pageSize = call.parameters[QueryParams.PARAM_PAGE_SIZE]?.toIntOrNull() ?:  Constants.DEFAULT_PAGE_SIZE
+            val pageSize = call.parameters[QueryParams.PARAM_PAGE_SIZE]?.toIntOrNull() ?: Constants.DEFAULT_PAGE_SIZE
 
-            if (!chatService.doesChatBelongToUser(chatId, call.userId)){
+            if (!chatService.doesChatBelongToUser(chatId, call.userId)) {
                 call.respond(HttpStatusCode.Forbidden)
                 return@get
             }
 
-            val message = chatService.getMessagesForChat(chatId,page, pageSize)
+            val message = chatService.getMessagesForChat(chatId, page, pageSize)
             call.respond(HttpStatusCode.OK, message)
         }
     }
 }
 
-fun Route.getChatsForUser(chatService: ChatService){
-    authenticate{
-        get("/api/chats"){
+fun Route.getChatsForUser(chatService: ChatService) {
+    authenticate {
+        get("/api/chats") {
             val chats = chatService.getChatsForUser(call.userId)
-            call.respond(HttpStatusCode.OK,chats)
+            call.respond(HttpStatusCode.OK, chats)
         }
     }
 }
 
-fun Route.chatWebSocket(chatController: ChatController){
-    authenticate{
+fun Route.chatWebSocket(chatController: ChatController) {
+    authenticate {
         webSocket("/api/chat/websocket") {
             println("Connecting via web socket")
+            chatController.onJoin(call.userId, this)
 
+            try {
+                incoming.consumeEach { frame ->
+                    kotlin.run {
+                        when (frame) {
+                            is Frame.Text -> {
+                                val frameText = frame.readText()
+                                val delimiterIndex = frameText.indexOf("#")
+                                if (delimiterIndex == -1) {
+                                    println("No delimiter found")
+                                    return@run
+                                }
+                                val type = frameText.substring(0, delimiterIndex).toIntOrNull()
+                                if (type == null){
+                                    println("Invalid format")
+                                    return@run
+                                }
+                                val json = frameText.substring(delimiterIndex + 1,frameText.length)
+                                handleWebSocket(call.userId, chatController,type, frameText, json)
+                            }
+                            else -> Unit
+                        }
+                    }
+                }
+            }catch (e: Exception){
+                e.printStackTrace()
+            }finally {
+                println("Disconnecting ${call.userId}")
+                chatController.onDisconnect(call.userId)
+            }
+        }
+    }
+}
+
+suspend fun handleWebSocket(
+    ownUserId: String,
+    chatController: ChatController,
+    type: Int,
+    frameText: String,
+    json:String
+){
+    val gson by inject<Gson>(Gson::class.java)
+    when(type){
+        WebSocketObject.MESSAGE.ordinal -> {
+            val message = gson.fromJsonOrNull(json, WsClientMessage::class.java) ?: return
+            println("Received message $message from $ownUserId")
+            chatController.sendMessage(ownUserId, gson, message)
         }
     }
 }
--- src/main/kotlin/com/example/service/chat/ChatController.kt
+++ src/main/kotlin/com/example/service/chat/ChatController.kt
@@ -1,4 +1,29 @@
 package com.example.service.chat
 
-class ChatController {
+import com.example.data.repository.chat.ChatRepository
+import com.google.gson.Gson
+import io.ktor.websocket.*
+import java.util.concurrent.ConcurrentHashMap
+
+class ChatController(
+    private val repository: ChatRepository
+) {
+    private val onlineUsers = ConcurrentHashMap<String, WebSocketSession>()
+
+    fun onJoin(userId: String, socket:WebSocketSession){
+        onlineUsers[userId] = socket
+    }
+
+    fun onDisconnect(userId: String){
+        if (onlineUsers.contains(userId)){
+            onlineUsers.remove(userId)
+        }
+    }
+
+//    suspend fun sendMessage(ownUserId: String, gson: Gson, message:WsClientMessage){
+//
+//    }
+
+
+
 }
